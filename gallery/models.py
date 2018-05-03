657a34faed6b9644591b0a0e06a1adca367027a2
ppolle <peter.m.polle@gmail.com>
2018-05-03 20:00:21 +0300

    created initial migrations

--- gallery/models.py
+++ gallery/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-	image_name = models.CharField(max_length = 60)
-	image_description = models.TextField()
-	# image_location = models.ForeignKey(Location)
-	# image_category = models.ForeignKey(Category)
-	image = models.ImageField(upload_to = 'images/')
 
 class Location(models.Model):
 	location = models.CharField(max_length = 60)
 
 class Category(models.Model):
 	category = models.CharField(max_length = 60)
+
+class Image(models.Model):
+	image_name = models.CharField(max_length = 60)
+	image_description = models.TextField()
+	image_location = models.ForeignKey(Location)
+	image_category = models.ForeignKey(Category)
+	image = models.ImageField(upload_to = 'images/')
+
