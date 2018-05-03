3179624e5a9ac6c036b412c9ac0f2061dd169b14
ppolle <peter.m.polle@gmail.com>
2018-05-03 19:50:30 +0300

    initial UI look with bootstrap3

--- gallery/models.py
+++ gallery/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Image(models.Model):
 	image_name = models.CharField(max_length = 60)
 	image_description = models.TextField()
-	image_location = models.ForeignKey(Location)
-	image_category = models.ForeignKey(Category)
+	# image_location = models.ForeignKey(Location)
+	# image_category = models.ForeignKey(Category)
 	image = models.ImageField(upload_to = 'images/')
 
 class Location(models.Model):
 	location = models.CharField(max_length = 60)
 
 class Category(models.Model):
-	category = models.Cahrfield(max_length = 60)
+	category = models.CharField(max_length = 60)
