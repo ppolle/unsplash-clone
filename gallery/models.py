1a718d97891f0a807217553a151786ec7dca7cf7
ppolle <peter.m.polle@gmail.com>
2018-05-04 10:18:09 +0300

    implemented image saving and displaying images on view functionality

--- gallery/models.py
+++ gallery/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class Location(models.Model):
 	location = models.CharField(max_length = 60)
 
+	def __str__(self):
+		return self.location
+
 class Category(models.Model):
 	category = models.CharField(max_length = 60)
+
+	def __str__(self):
+		return self.category
 
 class Image(models.Model):
 	image_name = models.CharField(max_length = 60)
@@ -14,4 +20,7 @@ class Image(models.Model):
 	image_location = models.ForeignKey(Location)
 	image_category = models.ForeignKey(Category)
 	image = models.ImageField(upload_to = 'images/')
+
+	def __str__(self):
+		return self.image_name
 
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
 def index(request):
-	return render(request,'index.html')
+	images = Image.objects.all()
+	return render(request,'index.html',{"images":images})
