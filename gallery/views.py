5456abfa1cb4678dd38787b3b9ff13045ef07006
ppolle <peter.m.polle@gmail.com>
2018-05-06 18:07:18 +0300

    updated navbar to show locations dropdown on which in turn redirect to location showing the images from a particular location

--- gallery/views.py
+++ gallery/views.py
@@ -11,13 +11,16 @@ def search_results(request):
 	if request.GET['search']:
 		search_term = request.GET.get("search")
 		images = Image.search_by_title(search_term)
+		imageLocations = Location.objects.all()
 		message = f"{search_term}"
 
-		return render(request,'search.html',{"message":message,"images":images})
+		return render(request,'search.html',{"message":message,"images":images,"imageLocations":imageLocations})
 	else:
 		message = "You haven't searched for any item"
-		return render(request,'search.html',{"message":message})
+		imageLocations = Location.objects.all()
+		return render(request,'search.html',{"message":message,"imageLocations":imageLocations})
 
 def location(request,location):
 	images = Image.filter_by_location(location)
-	return render(request,'location.html',{"images":images})
+	imageLocations = Location.objects.all()
+	return render(request,'location.html',{"images":images,"imageLocations":imageLocations})
